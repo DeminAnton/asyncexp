98d85ee4d0b49cbd71a22985c7ceed6b30d6bd94
DeminAnton <demantser@gmail.com>
2024-05-15 12:26:49 +0800

    wait, gather add

--- asyncexperiments/async_http.py
+++ asyncexperiments/async_http.py
@@ -1,7 +1,6 @@
 import asyncio
 import aiohttp
 
-from aiohttp import ClientSession
 from asyncexperiments.utils import async_timed
 from asyncexperiments.async_utils.async_utils import fetch_status
 
@@ -13,9 +12,12 @@ fetch_status = async_timed()(fetch_status)
 async def main():
     session_timeout = aiohttp.ClientTimeout(total=50, connect=10)
     async with aiohttp.ClientSession(timeout=session_timeout) as session:
-        url = 'https://www.example.com'
+        url = 'https://www.yandex.ru'
         requests = [fetch_status(session, url) for _ in range(1000)]
-        status_codes = await asyncio.gather(*requests)
-        print(status_codes)
-
+        status_codes = await asyncio.gather(*requests, return_exceptions=True)
+        exceptions = [res for res in status_codes if isinstance(res, Exception)]
+        success = [res for res in status_codes if not isinstance(res, Exception)]
+        print(f"Exceptions: {exceptions}")
+        print(f"Result codes: {success}")
 asyncio.run(main())
+
